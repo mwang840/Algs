367051bcc267f8a7404ece4ce15bcc0c977d272f
mwang840 <maxwang@udel.edu>
2023-02-07 15:06:27 -0500

    feat included null case

--- highestValList.java
+++ highestValList.java
@@ -1,5 +1,8 @@
 public class highestValList {
     public static int findHighestValue(int [] values){
+        if(values.length == 0){
+            return 0;
+        }
         int MAX = values[0];
         for(int i = 1; i < values.length; i++){
             if(values[i] > MAX){
@@ -11,9 +14,12 @@ public class highestValList {
 public static void main(String[] args) {
     int []numbers = {5, 1, 2, 15, 29, 25, 26};
     int []numbers2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int []noNumbers = {};
     //Should give me 29
     System.out.println(findHighestValue(numbers));
     //Should give me 10
     System.out.println(findHighestValue(numbers2));
+    //Should Return Null
+    System.out.println(findHighestValue(noNumbers));
 }
 }
