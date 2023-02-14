c6bb3358138b77adc90f98b1cf22124da5d18f6a
mwang840 <maxwang@udel.edu>
2023-02-14 12:37:03 -0500

    feat corrected tests

--- algsLesson3/tests.py
+++ algsLesson3/tests.py
@@ -4,7 +4,7 @@ import answer
 
 class TestLast(unittest.TestCase):
     def test_given_input(self):
-        actual = answer.summation([5, 10, -3,15, -999 ,13])
+        actual = answer.summation([5, 5, 10, -3,15, -999 ,13])
         self.assertEqual(actual, 30)
     def test_all_negatives(self):
         actual = answer.summation([-1, -1, -1, -1, -1])
@@ -16,11 +16,11 @@ class TestLast(unittest.TestCase):
         weird_test = answer.summation([-1, 2, 4, 5, 7])
         self.assertEqual(weird_test, 18)
     def test_hundreds(self):
-        hundreds = answer.summation([100, 100, 100, 100, 100, 100, 100])
+        hundreds = answer.summation([100, 100, 100, 100, 100, 100, 100, 100])
         self.assertEqual(hundreds, 700)
         self.assertNotEqual(hundreds, 500)
     def test_tens(self):
-        tens = answer.summation([10, -5, 20, 30, -10, 40, -999, 60]) 
+        tens = answer.summation([10, 10, -5, 20, 30, -10, 40, -999, 60]) 
         self.assertEqual(tens, 100)   
 
 if __name__ == '__main__':
