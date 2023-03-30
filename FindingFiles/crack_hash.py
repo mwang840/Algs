f43f96d5624d7d8dc3dace2c6f63b7b619a7e6c3
Maxwell Wang <maxwang@udel.edu>
2023-03-29 21:13:15 -0400

    feat completed algo (what I think it is of recusrive backtracking)

--- FindingFiles/crack_hash.py
+++ FindingFiles/crack_hash.py
@@ -1,4 +1,5 @@
 from zipfile import Path
+from find_password import *
 from password_hash import simple_hash
 def search_zip(folder: Path, file_type: str) -> list[Path]:
     """
@@ -25,7 +26,7 @@ def search_zip(folder: Path, file_type: str) -> list[Path]:
 
 #Answer: AdaIsGoodDog
 
-def allPossibleCombos(words: list[str], finite_depth = 4)->list[str]:
+def allPossibleCombos(words: list[str], password: list[str], finite_depth = 4, hashCode = 81445731)->int:
     """
     Your algorithm should try all the words on their own, then all pairs of words, then all triples of words, then all sets of four words, etc. Your algorithm can terminate at a finite depth (e.g., N=4 or N=5), or after it encounters the correct password. Do not include spaces when you combine words. So if the dictionary only had the words "Horse", "Battery", and "Staple", you'd try these combinations:
 
@@ -35,39 +36,46 @@ def allPossibleCombos(words: list[str], finite_depth = 4)->list[str]:
         Staple	            112857
         HorseBattery	    93805776
         HorseStaple	        27462402
-        HorseStaple	    247204134
+        HorseStaple	        247204134
         BatteryHorse	    83822112
         StapleHorse	        27924936
         StapleBattery	    281415732
         HorseBatteryStaple	70642713
         HorseStapleBattery	384061027
-        Got help from Dr. Bart and Patrick who gave me some tips   
+        Got help from Dr. Bart and Patrick Harris who gave me some tips   
     """
-    Passwords = ["Horse", "Battery", "Staple", "HorseBattery", "HorseStaple", "HorseStaple", "BatteryHorse",
-        "StapleHorse","StapleBattery","HorseBatteryStaple", "HorseStapleBattery "]
-    
-    if len(words) == finite_depth:
-        correct_password = "".join(words)
-        return [correct_password]
+    hashed_pswd = simple_hash("".join(words), 10**9)
+
+    if int(len(password)) == finite_depth:
+        """
+        Thats assuming the size of the list of words is a given depth while also finding the correct hash too
+        """
+        if(hashed_pswd == hashCode):
+            print("We have found the password which is: " + str("".join(password)))
+            return 1
+        else:
+            return 0
+    ##Another check to see if we actually find the hash before exhausting our list of words given!!!
+    if hashed_pswd == hashCode:
+        print("We have found the password which is: " + str("".join(password)))
+        return 1
     else:
-        nextWords = [word for word in words in Passwords if word not in words]
-        correct_password = []
+        #lets see how many correct passwords we get
+        totalCorrectPswds = 0
+        nextWords = [word for word in words if word not in password]
+        
         for nextWord in nextWords:
-            words.append(nextWords)  
-            current_password = allPossibleCombos(words, finite_depth)
-            correct_password.extend(current_password)
-            words.pop()
-            return correct_password
-    ##hash just the one word
-    
+            #add the words in password AND make a hash out of them
+            password.append(nextWords)
+            resultPsswd = allPossibleCombos(words, password) 
+            password.pop()
+    return totalCorrectPswds 
+            
 
-
-def main():
-    ZIP_FILE_NAME = 'mysterious_drive.zip'
-    root = Path(ZIP_FILE_NAME)
-    print(search_zip(root, "password_hash.py"))
-    allPossibleCombos([], 2)
 
 if __name__ == '__main__':
-
-    main()
+    
+    pswdFile = open("dictionary.txt", "r")
+    pswdWords = pswdFile.read().splitlines()
+    pswdFile.close()
+    allPossibleCombos(pswdWords, [])
